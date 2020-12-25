7f32cd3455d4e435d012d9d6f4bca5bcd96044e3
duct317 <duct@nevada.unr.edu>
2020-12-25 13:18:01 -0800

    add multicore for rvec, add windows support

--- bigsimr/rvec.py
+++ bigsimr/rvec.py
@@ -1,12 +1,14 @@
 import jax
 import numpy as np
-from bigsimr.cor_utils import cor_convert
+import scipy
+import os
 from bigsimr.cor_nearPSD import cor_nearPSD
+import multiprocessing as mp
 
 
-def _rmvuu(n, rho):
+def jax_rmvuu(n, rho):
     R = jax.device_put(np.array(rho))
-    m = jax.numpy.zeros(rho.shape[1])
+    m = jax.numpy.zeros(rho.shape[0])
 
     key = jax.random.PRNGKey(np.random.randint(1e6))
     size = [n]
@@ -17,22 +19,41 @@ def _rmvuu(n, rho):
     return jax.device_get(U)
 
 
-def rvec(n, rho, margins, type="pearson", ensure_PSD=False):
-    types = ['pearson', 'spearman', 'kendall']
-    if type not in types:
-        raise ValueError("Invalid type. Expected one of: %s" % types)
+def np_rmvuu(n, rho):
+    m = np.zeros(rho.shape[0])
 
-    if type == "spearman" or type == "kendall":
-        rho = cor_convert(rho, type + '_' + 'pearson')
+    np.random.seed(np.random.randint(1e6))
 
+    Z = np.random.multivariate_normal(m, rho, n)
+    U = scipy.stats.norm.cdf(Z)
+
+    return U
+
+
+def _u2m(u, margin):
+    return margin.ppf(u)
+
+
+def rvec(n, rho, margins, cores=1, ensure_PSD=False):
     if ensure_PSD:
-        rho = nearPD(rho)
-
-    U = _rmvuu(n, rho)
+        rho = cor_nearPSD(rho)
+    n = int(n)
     d = rho.shape[0]
 
-    rv = np.zeros_like(U)
-    for i in range(d):
-        rv[:, i] = margins[i].ppf(U[:, i])
+    if os.name == 'nt':
+        U = np_rmvuu(n, rho)
+    else:
+        U = jax_rmvuu(n, rho)
+
+    rv = np.empty_like(U)
+    if cores <= 1 or os.name == 'nt':
+        for i in range(d):
+            rv[:, i] = margins[i].ppf(U[:, i])
+    else:
+        pool = mp.Pool(processes=cores)
+        rv = [pool.apply_async(_u2m, args=(U[:, i], margins[i],)) for i in range(d)]
+        pool.close()
+        rv = [p.get() for p in rv]
+        rv = np.stack(rv, axis=1)
 
     return rv
